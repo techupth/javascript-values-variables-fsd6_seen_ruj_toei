42e14f9dc78bfc1359b9eb34cb2489aea83f5281
Puttapong <puttapong.lekk@gmail.com>
2024-05-09 13:33:21 +0700

    values variables assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let FirstName = "Karter";
+let LastName = "Anderson";
+let Address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const NetPrice = 15000;
+const amountrestaurant = 1500;
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+Address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+NetPrice = 5000;
+amountrestaurant = 2500;
