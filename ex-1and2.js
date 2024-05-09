1d616849ee6633e688647e50d03ecf465553177a
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-05-09 13:11:42 +0700

    ex 1&2

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,16 @@
-//Exercise #1: Variable Creation
-//Start coding here
+// Exercise #1: Variable Creation 
+// Start coding here 
+let Fname = "Karter";
+let Lname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
 
-//Exercise #2: Variable Reassignment
-//Start coding here
+const restauantName = "Chef's Table by Chef James";
+const totalBillAmount = 15000;
+const currentlyOpenRestaurants = 1500;
+
+// Exercise #2: Variable Reassignment
+// Start coding here
+address = "970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+totalBillAmount = 5000;
+currentlyOpenRestaurants = 2500;
+
